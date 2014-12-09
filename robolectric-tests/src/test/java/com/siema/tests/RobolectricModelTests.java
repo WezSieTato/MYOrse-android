277ffebf4c0717d0c4da73d68f5530467e6d44b5
WezSieTato <le0n.uo@gmail.com>
2014-12-09 15:29:59 +0100

    Robolectric dziala

--- robolectric-tests/src/test/java/com/siema/tests/RobolectricModelTests.java
+++ robolectric-tests/src/test/java/com/siema/tests/RobolectricModelTests.java
@@ -1,40 +1,52 @@
-package com.siema.morse.model;
-
-import android.test.InstrumentationTestCase;
+package com.siema.tests;
 
 /**
  * Created by marcin on 09/12/14.
  */
-public class ModelTest extends InstrumentationTestCase {
 
+import com.siema.RobolectricGradleTestRunner;
+import com.siema.morse.model.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(RobolectricGradleTestRunner.class)
+public class RobolectricModelTests {
+
+    @Test
     public void testDot() throws Exception {
         Char morse = new Dot();
         assertTrue(morse.isEmitSound());
         assertEquals(Char.defaultTime * 1, morse.getTime());
     }
 
+    @Test
     public void testDash() throws Exception {
         Char morse = new Dash();
         assertTrue(morse.isEmitSound());
         assertEquals(Char.defaultTime * 3, morse.getTime());
     }
 
+    @Test
     public void testDelayAfterMorseChar() throws Exception {
         Char morse = new DelayAfterMorseChar();
         assertFalse(morse.isEmitSound());
         assertEquals(Char.defaultTime * 1, morse.getTime());
     }
 
+    @Test
     public void testDelayAfterChar() throws Exception {
         Char morse = new DelayAfterChar();
         assertFalse(morse.isEmitSound());
         assertEquals(Char.defaultTime * 3, morse.getTime());
     }
 
+    @Test
     public void testDelayAfterWord() throws Exception {
         Char morse = new DelayAfterWord();
         assertFalse(morse.isEmitSound());
         assertEquals(Char.defaultTime * 9, morse.getTime());
     }
-
 }
